d11e7d9d71ddff21dce078e9817c385fdc91d2cb
Liz Chaddock <lizchaddock@bloodjourney-lm.corp.yahoo.com>
2015-09-14 17:13:58 -0700

    Initial Commit

---
+++ rottenTomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rottenTomatoes
+//
+//  Created by Liz Chaddock on 9/14/15.
+//  Copyright (c) 2015 Liz Chaddock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ rottenTomatoes/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  rottenTomatoes
+//
+//  Created by Liz Chaddock on 9/14/15.
+//  Copyright (c) 2015 Liz Chaddock. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
